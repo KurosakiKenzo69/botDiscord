7157e57301a1c6b8cefc63a1d3e8e27a81c99013
Kenzo VONGKINGKEO <freeken69@gmail.com>
2023-12-12 17:05:52 +0100

    config

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 module.exports = {
-    token : "MTE4NDAzMjkzNTczNzgyNzM0OQ.GC4qjd.f3xEzt3P4AvIDGUIKw7NLt5cAUtR0QIzxXrrEg"
+    token: process.env.DISCORD_TOKEN // Récupère le token à partir des variables d'environnement
 };
